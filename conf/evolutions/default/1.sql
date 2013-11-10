0cb6838e4b02b4c214d101e885fee5f02a84ba58
Eamonn de Leastar <edeleastar@gmail.com>
2013-11-10 07:44:20 +0000

    1st default evolution

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,29 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table my_user (
+  id                        bigint not null,
+  firstname                 varchar(255),
+  lastname                  varchar(255),
+  email                     varchar(255),
+  password                  varchar(255),
+  constraint pk_my_user primary key (id))
+;
+
+create sequence my_user_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists my_user;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists my_user_seq;
+
