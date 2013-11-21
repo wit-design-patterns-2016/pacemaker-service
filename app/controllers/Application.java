bbbc55b2c39335ddc6d0787f6751978f3dcfbd5a
Eamonn de Leastar <edeleastar@gmail.com>
2013-11-21 14:02:07 +0000

    Semantic ui - first draft including user list + user form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,31 @@
 package controllers;
 
-import play.*;
+import java.util.List;
+import models.User;
 import play.mvc.*;
-
 import views.html.*;
+import play.data.Form;
 
-public class Application extends Controller {
-
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-
+public class Application extends Controller
+{
+  private static final Form<User> userForm = Form.form(User.class);
+  
+  public static Result index()
+  {
+    List<User> users = User.findAll();
+    return ok(index.render(users));
+  }
+  
+  public static Result addUser()
+  {
+    return ok(adduser.render());
+  }
+  
+  public static Result submitUser()
+  {
+    Form<User> boundForm = userForm.bindFromRequest();
+    User user = boundForm.get();
+    user.save();
+    return redirect ("/");
+  }
 }
